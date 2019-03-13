99e357288de98db782d8f2f5d88969bd87006917
Andrew B Coathup <andrew@coathup.net>
2019-03-14 07:51:39 +1100

    Updated horses

--- bundle.js
+++ bundle.js
@@ -3644,8 +3644,34 @@ module.exports = function () {
         {color: 'DimGrey', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Grey'},
         {color: 'Lavender', mane: 'Purple', tail: 'Purple', shoes: 'Indigo', eye: 'Pink', unicorn: 'Gold', wings: 'GoldenRod'},
         {color: 'DarkBlue', mane: 'DarkSlateGrey', tail: 'DarkSlateGrey', shoes: 'Black', eye: 'Blue', wings: 'Yellow'},
-        {color: 'Coral', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'}       
-     
+        {color: 'Coral', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Purple', mane: 'Gold', tail: 'Gold', shoes: 'Black', eye: 'Orange', unicorn: 'GoldenRod', wings: 'GoldenRod'}, // KJC
+        {color: 'Orange', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'} , // ABC
+        {color: 'DarkCyan', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink', unicorn: 'GoldenRod'} , // AMC
+        {color: 'DeepPink', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'} , // S
+        {color: 'AliceBlue ', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'} , // K
+        {color: 'CornSilk', mane: 'Grey', tail: 'Grey', shoes: 'Grey', eye: 'Pink', wings: 'GoldenRod'} , // SF 
+        {color: 'Azure', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'} , // X
+        {color: 'Ivory', mane: 'DarkViolet', tail: 'DarkViolet', shoes: 'Black', eye: 'Pink'} , // T
+        {color: 'Indigo', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},  // A
+        {color: 'AntiqueWhite', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Aqua', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Aquamarine', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Beige', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Bisque', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'BlanchedAlmond', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Blue', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'BlueViolet', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Brown', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'BurlyWood', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'CadetBlue', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Chartreuse', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Chocolate', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'CornflowerBlue', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Crimson', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'Cyan', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'},
+        {color: 'DarkBlue', mane: 'Black', tail: 'Black', shoes: 'Black', eye: 'Pink'}
+        
     ]
 }
 },{}],38:[function(require,module,exports){
