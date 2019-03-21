3195e2b3beb0289b0798e548bcdf998344473e82
Andrew B Coathup <andrew@coathup.net>
2019-03-21 18:17:25 +1100

    Update header

--- templates/main.js
+++ templates/main.js
@@ -15,7 +15,7 @@ module.exports = function (state, emit) {
     
     <!-- Top menu on small screens -->
     <header class="w3-container w3-top w3-white w3-xlarge w3-padding-16">
-      <span class="w3-left w3-padding">SOME NAME</span>
+      <span class="w3-left w3-padding">BLOCK HORSES</span>
       <a href="javascript:void(0)" class="w3-right w3-button w3-white" onclick="w3_open()">☰</a>
     </header>
     
